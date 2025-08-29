9d36f4568c49c16e16348adf129e17e4256c77c8
Marco Bacci <marcobacci94@gmail.com>
2025-08-29 17:40:12 +0200

    feat: create structure

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,34 @@ function App() {
   return (
     <>
       <div className="container">
-        <h1 className="text-center m-3">LISTA DI ARTICOLI </h1>
-        {articles.map((article) => {
-          return (
-            <ul class="list-group">
-              <li class="list-group-item">{article}</li>
-            </ul>
-          );
-        })}
+        <div className="row">
+          <div className="col-6">
+            <h2 className="text-center m-3">LISTA TITOLI </h2>
+            {articles.map((article) => {
+              return (
+                <ul class="list-group">
+                  <li class="list-group-item">{article}</li>
+                </ul>
+              );
+            })}
+          </div>
+          <div className="col-6">
+            <h2 className="text-center m-3">NUOVO TITOLO</h2>
+            <form className="mt-5">
+              <div className="form d-flex gap-3">
+                <input
+                  class="form-control form-control-lg"
+                  type="text"
+                  placeholder="Aggiungi un nuovo titolo di un articolo"
+                  aria-label=".form-control-lg example"
+                ></input>
+                <button type="button" class="btn btn-outline-dark">
+                  Dark
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   );
